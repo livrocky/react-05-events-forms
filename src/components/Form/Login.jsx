994c4f33b6c42f5f7a0654de46b160e0ec531524
rokas <rokasra@gmail.com>
2022-06-03 11:38:57 +0300

    form

--- src/components/Form/Login.jsx
+++ src/components/Form/Login.jsx
@@ -7,9 +7,6 @@ function Login() {
   const [showUsername, setShowUsername] = useState(false);
   const [showPassword, setShowPassword] = useState(false);
 
-  /**
-   * take input value from input and update state with it
-   */
   function usernameEnterHandler(event) {
     // console.log('entering value to input');
     // console.log(event.target); // ===  <input type='text' placeholder='Login here' />
@@ -28,19 +25,25 @@ function Login() {
 
   return (
     <div className='login'>
-      <input onChange={usernameEnterHandler} value={usernameValue} type='text' placeholder='Login here' />
-      <input onChange={passwordEnterHandler} value={passwordValue} type='password' placeholder='Enter Password Here' />
-      <h3>Jus ivedete: {usernameValue}</h3>
+      <form onSubmit={sendValues}>
+        <input onChange={usernameEnterHandler} value={usernameValue} type='text' placeholder='Login here' />
+        <input onChange={passwordEnterHandler} value={passwordValue} type='password' placeholder='Enter Password Here' />
+        <h3>Jus ivedete: {usernameValue}</h3>
 
-      <button onClick={sendValues}>send</button>
-      {showUsername && <h2>{usernameValue}</h2>}
-      {showUsername && <h2>{usernameValue}</h2>}
+        <button onClick={sendValues}>send</button>
+        {showUsername && <h2>{usernameValue}</h2>}
+        {showUsername && <h2>{usernameValue}</h2>}
+      </form>
     </div>
   );
 
-  function sendValues() {
+  function sendValues(event) {
+    event.preventDefault();
     // send values
     // pagaminti objekta is input reiksmiu
+    // kai mum reikia reiksmiu mes imam is state
+    // nes state yra sinkronizuota su inputais
+    if (usernameValue === '') return;
     const loginObj = {
       username: usernameValue,
       password: passwordValue,
